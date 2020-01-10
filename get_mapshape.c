07532596652320b87e8aef3d5bb1c9b812be3829
Mohamed Ait si m hand <mait-si-@e1r10p11.1337.ma>
2020-01-10 13:31:45 +0100

    auto-commit

--- get_mapshape.c
+++ get_mapshape.c
@@ -6,40 +6,88 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 10:17:46 by mait-si-          #+#    #+#             */
-/*   Updated: 2020/01/10 11:30:14 by mait-si-         ###   ########.fr       */
+/*   Updated: 2020/01/10 13:31:43 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cub3d.h"
 
-char	**get_mapshape(char *file)
+int		line_counter(char *line)
 {
-	int		fd;
-	char	*line;
-	int		height;
-	int		width;
+	int		len;
 
-	height = 0;
-	width = 0;
+	len = 0;
+	while (*line)
+		if (*line++ == ' ')
+			continue ;
+		else
+			len++;
+	return (len);
+}
+
+int		get_height(char *file)
+{
+	int		height;
+	char	*line;
+	int		fd;
+
+	height = 1;
 	if ((fd = open(file, O_RDONLY)) < 0)
 		ft_puterror("This file is not exist.");
 	while (get_next_line(fd, &line))
 	{
 		if (*line != '1')
 			continue ;
-		while (*line)
-		{
-			if (*line == '1')
-				width++;
-			line++;
-			while (*line == ' ')
-				line++;
-		}
 		height++;
 	}
-	ft_putnbr(width);
+	return (height);
+}
+
+int		get_width(char *file)
+{
+	int		width;
+	char	*line;
+	int		fd;
+	int		i;
+
+	if ((fd = open(file, O_RDONLY)) < 0)
+		ft_puterror("This file is not exist.");
+	while (get_next_line(fd, &line) && *line != '1')
+		continue;
+	width = line_counter(line);
+	while (get_next_line(fd, &line))
+	{
+		i = 0;
+		while (*line)
+			if (*line++ == ' ')
+				continue ;
+			else
+				i++;
+		if (i != width)
+			ft_puterror("Fix your Map");
+	}
+	if (line_counter(line) != width)
+		ft_puterror("Fix your Map");
+	return (width);
+}
+
+char	**get_mapshape(char *file)
+{
+	int		fd;
+	char	*line;
+	// char	**mapshape;
+
+	if ((fd = open(file, O_RDONLY)) < 0)
+		ft_puterror("This file is not exist.");
+	// mapshape = ft_calloc(get_height(file), 1);
+	while (get_next_line(fd, &line))
+	{
+		if (*line != '1')
+			continue ;
+	}
+	ft_putnbr(get_width(file));
 	ft_putstr(", ");
-	ft_putnbr(height);
+	ft_putnbr(get_height(file));
 	exit(1);
 	return (ft_split(line, '\n'));
 }
