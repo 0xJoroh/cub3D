11f0c4a7f313018c5d94b9c5bd6f6706e42cedce
Mohamed Ait si m hand <mait-si-@e2r1p11.1337.ma>
2020-03-12 23:28:15 +0100

    auto-commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/04 21:24:33 by mait-si-          #+#    #+#             */
-/*   Updated: 2020/03/10 13:28:07 by mait-si-         ###   ########.fr       */
+/*   Updated: 2020/03/12 16:33:59 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		main(int argc, char *argv[])
 		ft_puterror("you have to pass at least two argument");
 	g_sprites = ft_calloc(1, sizeof(t_sprite*));
 	setup(argv[1]);
-	if (argc == 3)
+	if (argc == 3 && ft_strcmp("––save", argv[2]))
 		bmp(argv[2]);
 	mlx_hook(g_map.win_ptr, 2, 0, key_press, (void*)0);
 	mlx_hook(g_map.win_ptr, 3, 0, key_releas, (void*)0);
